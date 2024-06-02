02897479729f656be41dfc3c96ae8f6e1c3b0ddf
Krishna Manoj PVR <pvrkmsbunny.com>
2024-06-02 23:08:32 +0530

    45

---
+++ os_lab_4/4d_signal.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<signal.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<stdlib.h>
+#include<sys/wait.h>
+
+int main(){
+    pid_t pid = fork(); 
+    if(pid == -1){
+        printf("Error : Fork() is failed ");
+        exit(1);
+    }
+    if(pid == 0){
+        printf("Child process is running...\n");
+        while(1){
+            printf("child is working....");
+            sleep(1);
+        }
+    }
+    else{
+        printf("Parent process started\n");
+        sleep(2);
+        if(kill(pid,SIGSTOP)==-1){
+             perror("Failed to suspend child process");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Parent has suspended the child process\n");
+        sleep(2);
+        if(kill(pid,SIGCONT)==-1){
+            perror("Failed to resume child process");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Parent has resumed the child process\n");
+        sleep(2);
+
+        if(kill(pid,SIGKILL)==-1){
+            perror("Failed to terminate the child process");
+        }
+        printf("Parent has Terminated the child\n");
+
+        if(wait(NULL)==-1){
+            printf("failed to wait for child..");
+        }
+
+        printf("Parent process is finished");
+
+
+    }
+    
+return 0;
+}
