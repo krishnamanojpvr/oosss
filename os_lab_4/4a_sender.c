98ac618e2b4c06b95e6c8447c33a0c6b5e0b15bf
Krishna Manoj PVR <pvrkmsbunny.com>
2024-06-11 09:48:25 +0530

    os_lab_4

--- os_lab_4/4a_sender.c
+++ os_lab_4/4a_sender.c
@@ -5,6 +5,6 @@ int main()
 {
     int res, n;
     res = open("fifo1", O_WRONLY);
-    write(res, "IPC communication using named pipe", 35);
+    write(res, "Hello World", 12);
     printf("Sender Process %d sent the data\n", getpid());
 }
